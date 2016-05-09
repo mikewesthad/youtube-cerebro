4df5672f1b00f98dcac4384c2b80cafc6793197f
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-08 23:23:03 -0500

    Helper functions for manipulating DOM (used in interactive-3d-video.js)

--- js/helper-functions.js
+++ js/helper-functions.js
@@ -1,3 +1,23 @@
+function forEachInObject(object, iterationFunction) {
+	if (!object) return;
+	for (var key in object) {
+		if (!object.hasOwnProperty(key)) continue;
+		iterationFunction(key, object[key], object);
+	}
+}
+
+function createElement(tagName, style, attributes, parent) {
+	var element = document.createElement(tagName);
+	forEachInObject(style, function (key, val) {
+		element.style[key] = val;
+	});
+	forEachInObject(attributes, function (key, val) {
+		element[key] = val;
+	});
+	if (parent) parent.appendChild(element);
+	return element;
+}
+
 // Shuffle array in-place using Fisher-Yates shuffle:
 // 	https://bost.ocks.org/mike/shuffle/
 function shuffleArray(array) {
